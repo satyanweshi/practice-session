43ea0dbcafe89378f182f664c3019003c6c2b329
Varun Sharma <sharma.varunsh@gmail.com>
2017-06-15 22:37:11 +0530

    Changes to quick sort

--- quick-sort-vertx/src/main/java/com/facpro/algo/QuickSort.java
+++ quick-sort-vertx/src/main/java/com/facpro/algo/QuickSort.java
@@ -1,14 +1,85 @@
 import com.facpro.algo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.example.util.Runner;
+
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class QuickSort extends AbstractVerticle {
 
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(QuickSort.class);
+  }
+
   @Override
   public void start(){
-    // Create an HTTP server which simply returns "Hello World!" to each request.
-    // If a configuration is set it get the specified name
-    String name = config().getString("name", "World");
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello " + name + "!")).listen(8080);
+    BufferedReader br = null;
+
+        try {
+          
+            br = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+
+                System.out.print("Enter something : ");
+                String input = br.readLine();
+
+                if ("q".equals(input)) {
+                    System.out.println("Exit!");
+                    System.exit(0);
+                }
+
+                System.out.println("input : " + input);
+                System.out.println("-----------\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+  }
+
+
+  private List quickSort(List<Integer> inpArr, int low, int high){
+    if(inpArr != null && inpArr.size() > 0){
+      if(low < high){
+        int p = partition(inpArr, low, high);
+        quickSort(inpArr, low, p-1);
+        quickSort(inpArr, p+1, high);
+        inpArr.forEach(System.out::println);
+      }
+    }
+  }
+
+  private int partition (List<Integer> inpArr, int low, int high){
+    int pivot = inpArr.get(high);
+    int i = low - 1;
+    for(j = low; j < high-1; j++){
+      if(inpArr.get(j) < pivot){
+        i++;
+        swap(inpArr, i, j);
+      }
+    }
+    swap(inpArr, i+1, high);
+    return (i+1);
+  }
+
+  private void swap (List<Integer> inpArr, int i, int j){
+    int swap = inpArr.get(i);
+    inpArr.set(i,inpArr.get(j));
+    inpArr.set(j,swap);
   }
 }
