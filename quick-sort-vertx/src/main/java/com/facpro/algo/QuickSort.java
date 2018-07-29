d4e3ceead54ded4622553dac0bbcc24f48032435
Varun Sharma <sharma.varunsh@gmail.com>
2018-07-29 20:30:46 +0530

    Old code

--- quick-sort-vertx/src/main/java/com/facpro/algo/QuickSort.java
+++ quick-sort-vertx/src/main/java/com/facpro/algo/QuickSort.java
@@ -1,9 +1,11 @@
-import com.facpro.algo;
+package com.facpro.algo;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.example.util.Runner;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,68 +14,75 @@ import java.io.InputStreamReader;
 
 public class QuickSort extends AbstractVerticle {
 
-  // Convenience method so you can run it in your IDE
-  public static void main(String[] args) {
-    Runner.runExample(QuickSort.class);
-  }
-
   @Override
   public void start(){
-    BufferedReader br = null;
 
-        try {
-          
-            br = new BufferedReader(new InputStreamReader(System.in));
+                vertx.executeBlocking(future -> {
+                  BufferedReader br = null;
+                  try{
+                    System.out.print("Enter comma separated integer array : ");
+                    br = new BufferedReader(new InputStreamReader(System.in));
+                    String input = br.readLine();
 
-            while (true) {
+                    /*if ("q".equals(input)) {
+                        System.out.println("Exit!");
+                        break;
+                    }*/
 
-                System.out.print("Enter something : ");
-                String input = br.readLine();
+                    System.out.println("input : " + input);
+                    System.out.println("-----------\n");
+                    future.complete(input);
+                  }catch (IOException e) {
+                      e.printStackTrace();
+                  } finally {
+                      if (br != null) {
+                          try {
+                              br.close();
+                          } catch (IOException e) {
+                              e.printStackTrace();
+                          }
+                      }
+                  }
 
-                if ("q".equals(input)) {
-                    System.out.println("Exit!");
-                    System.exit(0);
-                }
-
-                System.out.println("input : " + input);
-                System.out.println("-----------\n");
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+                }, res -> {
+                  try{
+                    String input = (String)res.result();
+                    String[] intStrArr = input.split(",");
+                    List<Integer> inpArr = new ArrayList<Integer>();
+                    Arrays.stream(intStrArr).forEach(intArray -> inpArr.add(Integer.parseInt(intArray)));
+                    quickSort(inpArr, 0, inpArr.size());
+                    inpArr.forEach(item -> System.out.print(item + ", "));
+                  } catch(NumberFormatException e){
+                     e.printStackTrace();
+                  }
+                });
   }
 
 
   private List quickSort(List<Integer> inpArr, int low, int high){
     if(inpArr != null && inpArr.size() > 0){
-      if(low < high){
+      if((low + 1) < high){
         int p = partition(inpArr, low, high);
-        quickSort(inpArr, low, p-1);
+        inpArr.forEach(item -> System.out.print(item + ", "));
+        System.out.println();
+        quickSort(inpArr, low, p);
         quickSort(inpArr, p+1, high);
-        inpArr.forEach(System.out::println);
       }
     }
+    return inpArr;
   }
 
   private int partition (List<Integer> inpArr, int low, int high){
-    int pivot = inpArr.get(high);
+    int pivot = inpArr.get(high-1);
     int i = low - 1;
-    for(j = low; j < high-1; j++){
+    for(int j = low; j < high-2; j++){
       if(inpArr.get(j) < pivot){
         i++;
         swap(inpArr, i, j);
       }
     }
-    swap(inpArr, i+1, high);
+    System.out.println(pivot);
+    swap(inpArr, i+1, high-1);
     return (i+1);
   }
 
