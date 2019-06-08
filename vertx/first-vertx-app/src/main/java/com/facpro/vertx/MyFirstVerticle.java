8b7e9b513845dbad44e3c7145e19098d6ccbfa1c
Varun Sharma <sharma.varunsh@gmail.com>
2019-06-08 20:14:59 +0530

    new code in vertx and react

---
+++ vertx/first-vertx-app/src/main/java/com/facpro/vertx/MyFirstVerticle.java
@@ -0,0 +1,28 @@
+package com.facpro.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyFirstVerticle extends AbstractVerticle
+{
+    @Override
+  public void start(Future<Void> fut) {
+    vertx
+        .createHttpServer()
+        .requestHandler(r -> {
+          r.response().end("<h1>Hello from my first " +
+              "Vert.x 3 application</h1>");
+        })
+        .listen(8080, result -> {
+          if (result.succeeded()) {
+            fut.complete();
+          } else {
+            fut.fail(result.cause());
+          }
+        });
+  }
+}
